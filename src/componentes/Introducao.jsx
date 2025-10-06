071e5c6831df705a52633155920b781c26b438f6
mikael <mikaeljuliao56@gmail.com>
2025-10-06 16:11:40 -0300

    bug da borda ocupando 100% do elemento pai e não do conteudo corrigido

--- src/componentes/Introducao.jsx
+++ src/componentes/Introducao.jsx
@@ -12,14 +12,16 @@ export default function Introducao () {
 
         <div className='w-auto gap-6 sm:w-1/2 mt-12 lg-w-2/3 flex flex-col md:mt-0 md:pl-4'>
            
-           <div className='w-full flex justify-center'>
-            <div className='flex  gap-1 px-1 py-2 text-xl border border-sky-800 
-            rounded-3xl
+         
+            <div className=' flex gap-1 px-1 py-2 text-xl border border-sky-800 
+            rounded-3xl self-start w-fit
              text-sky-400 font-semibold'>
+            {/* self-start: impede o flex de esticar o elemento e o alinha á esquerda
+                w-fit: faz o elemento ocupar somente o espaço do conteúdo inteiro, sem 100% de largura */}  
                <i class='bx bxl-sketch '></i>
       Front-end Developer Portifolio
             </div>
-            </div> 
+         
 
             <h1 className='text-3xl md:text-6xl font-bold leading-tight mt-4'
             >Forncendo <span className=' bg-gradient-to-r from-sky-400 via-blue-500 to-purple-600 
@@ -31,7 +33,8 @@ export default function Introducao () {
               Sou desenvolvedor front-end com experiência na criação de sites responsivos e interfaces modernas.
        Confira minhas habilidades..</p>
 
-       <button className='flex items-end gap-2 text-xl border border-sky-800 px-2 py-3 mt-5 rounded-2xl'>
+       <button className='flex items-end gap-1 text-xl border border-sky-800 px-2 py-3 mt-5
+        rounded-2xl w-fit'>
         <i class='bx bx-send'></i>Entre em contato comigo</button>
         </div>
 
