a33e50dcc25faaf880a346a6b0dbdec99d12091c
mikael <mikaeljuliao56@gmail.com>
2025-12-05 12:22:23 -0300

    Descrição dos projetos e seus links para website atualizados

--- src/componentes/Projetos.jsx
+++ src/componentes/Projetos.jsx
@@ -51,11 +51,11 @@ const projeto3 = [
             <h1 className='font-bold text-xl mt-6 mb-3 
             '> <span className=" bg-gradient-to-r from-sky-400 via-blue-500 to-purple-600 
             bg-clip-text text-transparent animate-gradient-x duration-300 ease-in-out">Gerenciador</span>  de Medicamentos</h1>
-            <p className='font-semibold '>Aplicação freelancer real para controle de medicamentos, com CRUD completo, 
+            <p className='font-semibold '>Aplicação freelancer para controle de medicamentos, com CRUD completo, 
             histórico de registros e alerta sonoro para horários. Simulação de API REST com JSON 
             local para exibição dinâmica dos dados.
        
-           Skills:<strong className="gradient-skills">JavaScript (ES6+)</strong>,<strong className="border-b-2 border-sky-600">HTML</strong>, 
+           Skills:{' '} <strong className="gradient-skills">JavaScript (ES6+)</strong>,<strong className="border-b-2 border-sky-600">HTML</strong>, 
            <strong className="border-b-2 border-sky-600">CSS</strong>,<strong className="border-b-2 border-sky-600"> JSON</strong>,
            <strong className="border-b-2 border-sky-600"> localStorage</strong>, <strong className="border-b-2 border-sky-600"> Git/GitHub,</strong>
            <strong className="border-b-2 border-sky-600"> UX/UI</strong>, 
@@ -82,21 +82,29 @@ const projeto3 = [
              via-blue-500 to-purple-600 
             bg-clip-text text-transparent animate-gradient-x duration-300 ease-in-out">Portfólio</span> Pessoal
            </h1>
-            <p className='font-semibol'>Aplicação freelancer real para controle de medicamentos, com CRUD completo, 
-            histórico de registros e alerta sonoro para horários. Simulação de API REST com JSON 
-            local para exibição dinâmica dos dados.
-       
-           Skills:<strong className="border-b-2 border-sky-600">JavaScript (ES6+)</strong>,<strong className="border-b-2 border-sky-600">HTML</strong>, 
-           <strong className="border-b-2 border-sky-600">CSS</strong>,<strong className="border-b-2 border-sky-600"> JSON</strong>,
-           <strong className="border-b-2 border-sky-600"> localStorage</strong>, <strong className="border-b-2 border-sky-600"> Git/GitHub,</strong>
-           <strong className="border-b-2 border-sky-600"> UX/UI</strong>, 
-            <strong className="border-b-2 border-sky-600">Responsividade</strong>,<strong className="border-b-2 border-sky-600"> Acessibilidade</strong>,
-            <strong className="border-b-2 border-sky-600"> Clean Code </strong></p>   
+            <p className='font-semibol'>Meu site pessoal onde apresento meus projetos, habilidades e 
+              atuação como front-end. Estruturado para ser rápido, moderno e 100% responsivo, 
+              com design focado em identidade visual, navegação fluida e componentes 
+              reutilizáveis desenvolvidos em React.
+       Skills: {' '}
+        <strong className="border-b-2 border-sky-600">React</strong>,{' '}
+        <strong className="border-b-2 border-sky-600">JavaScript (ES6+)</strong>,{' '}
+        <strong className="border-b-2 border-sky-600">HTML5</strong>,{' '}
+        <strong className="border-b-2 border-sky-600">CSS3</strong>,{' '}
+        <strong className="border-b-2 border-sky-600">Tailwind CSS</strong>,{' '}
+        <strong className="border-b-2 border-sky-600">Vite</strong>,{' '}
+        <strong className="border-b-2 border-sky-600">Git/GitHub</strong>,{' '}
+        <strong className="border-b-2 border-sky-600">UX/UI</strong>,{' '}
+        <strong className="border-b-2 border-sky-600">Responsividade</strong>,{' '}
+        <strong className="border-b-2 border-sky-600">Componentização</strong>,{' '}
+        <strong className="border-b-2 border-sky-600">Acessibilidade (A11y)</strong>,{' '}
+        <strong className="border-b-2 border-sky-600">Clean Code</strong>
+        </p>   
 
             <button className='flex items-end text-lg shadow-[0_0_5px_#38bdf8] border border-sky-800 px-2 py-1 mt-4
         rounded-lg transition-all duration-300 hover:rounded-md hover:text-sky-400 hover:shadow-[0_0_10px_#38bdf8]'>
             <i className='bx bx-link-external m-auto p-1'></i> 
-            <a href="https://lembre-med.netlify.app/" target="_blank">Websit</a></button>
+            <a href="https://portfolio-mikael-juliao-dev.netlify.app/" target="_blank">Websit</a></button>
         </div>
 
 
@@ -115,21 +123,29 @@ const projeto3 = [
             <h1 className='font-bold text-xl mt-6 mb-3'>Controle de  <span className=" bg-gradient-to-r from-sky-400
              via-blue-500 to-purple-600 
             bg-clip-text text-transparent animate-gradient-x duration-300 ease-in-out">Gastos</span>  </h1>
-            <p className='font-semibol'>Aplicação freelancer real para controle de medicamentos, com CRUD completo, 
-            histórico de registros e alerta sonoro para horários. Simulação de API REST com JSON 
-            local para exibição dinâmica dos dados.
+            <p className='font-semibol'>Aplicação web para gerenciamento financeiro pessoal, com CRUD completo, filtros dinâmicos e 
+              saldo atualizado em tempo real. Conta com gráfico interativo de receitas x despesas e 
+              layout responsivo desenvolvido seguindo boas práticas de Clean Code.
        
-           Skills:<strong className="border-b-2 border-sky-600">JavaScript (ES6+)</strong>,<strong className="border-b-2 border-sky-600">HTML</strong>, 
-           <strong className="border-b-2 border-sky-600">CSS</strong>,<strong className="border-b-2 border-sky-600"> JSON</strong>,
-           <strong className="border-b-2 border-sky-600"> localStorage</strong>, <strong className="border-b-2 border-sky-600"> Git/GitHub,</strong>
-           <strong className="border-b-2 border-sky-600"> UX/UI</strong>, 
-            <strong className="border-b-2 border-sky-600">Responsividade</strong>,<strong className="border-b-2 border-sky-600"> Acessibilidade</strong>,
-            <strong className="border-b-2 border-sky-600"> Clean Code </strong></p>   
+          Skills:{' '}
+<strong className="border-b-2 border-sky-600">React</strong>,{' '}
+<strong className="border-b-2 border-sky-600">JavaScript (ES6+)</strong>,{' '}
+<strong className="border-b-2 border-sky-600">Recharts</strong>,{' '}
+<strong className="border-b-2 border-sky-600">Bootstrap</strong>,{' '}
+<strong className="border-b-2 border-sky-600">HTML5</strong>,{' '}
+<strong className="border-b-2 border-sky-600">CSS3</strong>,{' '}
+<strong className="border-b-2 border-sky-600">localStorage</strong>,{' '}
+<strong className="border-b-2 border-sky-600">Git/GitHub</strong>,{' '}
+<strong className="border-b-2 border-sky-600">UX/UI</strong>,{' '}
+<strong className="border-b-2 border-sky-600">Responsividade</strong>,{' '}
+<strong className="border-b-2 border-sky-600">Acessibilidade</strong>,{' '}
+<strong className="border-b-2 border-sky-600">Clean Code</strong>
+</p>   
 
             <button className='flex items-end text-lg shadow-[0_0_5px_#38bdf8] border border-sky-800 px-2 py-1 mt-4
         rounded-lg transition-all duration-300 hover:rounded-md hover:text-sky-400 hover:shadow-[0_0_10px_#38bdf8]'>
             <i className='bx bx-link-external m-auto p-1 '></i> 
-            <a href="https://lembre-med.netlify.app/" target="_blank">Websit</a></button>
+            <a href="https://lista-de-gasto.netlify.app/" target="_blank">Websit</a></button>
         </div>  
     </div>
 
