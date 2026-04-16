3b8e33700c41788311d04be3114c6534fc99c85c
mikael <mikaeljuliao56@gmail.com>
2026-04-16 01:29:56 -0300

    adicionado mais um projeto

--- src/componentes/Projetos.jsx
+++ src/componentes/Projetos.jsx
@@ -47,6 +47,19 @@ const projeto4 = [
 ];
 
 
+const projeto5 = [
+  "/imagem/ecommerce-store-1.png",
+  "/imagem/ecommerce-store-2.png",
+  "/imagem/ecommerce-store-3.png",
+  "/imagem/ecommerce-store-4.png",
+  "/imagem/ecommerce-store-5.png",
+  "/imagem/ecommerce-store-6.png",
+  "/imagem/ecommerce-store-7.png",
+  "/imagem/ecommerce-store-8.png",
+  "/imagem/ecommerce-store-9.png"
+];
+
+
   return (
     <section id='projeto' className='text-white w-full min-h-screen flex flex-col items-center justify-center py-32
     bg-gradient-to-br 
@@ -208,6 +221,50 @@ to-[#020b1f]'>
             <i className='bx bx-link-external m-auto p-1'></i> 
             <a href="https://ecommerce-store-op.netlify.app/" target="_blank">Websit</a></button>
         </div>
+
+
+           {/* Card 4 */}   
+
+      <div className="">
+     <div className="border-2 border-sky-400 rounded-xl overflow-hidden shadow-[0_0_10px_#38bdf8] hover:shadow-[0_0_20px_#1e40ff]">
+     <Carrosel images={projeto4} />
+   </div>
+
+        <div className="projeto-inf fadeInRight">
+          
+            <h1 className='font-bold text-xl mt-6 mb-3'><span className=" bg-gradient-to-r from-sky-400
+             via-blue-500 to-purple-600 
+            bg-clip-text text-transparent animate-gradient-x duration-300 ease-in-out"> E-commerce
+      </span> Store
+           </h1>
+            <p className='font-semibol'> Template open source que simula uma loja virtual real. Produtos, categorias e
+      promoções são controlados por dados JSON, permitindo atualização rápida e
+      renderização automática da interface.
+       Skills: {' '}
+        <strong className="border-b-2 border-sky-600">React</strong>,{' '}
+        <strong className="border-b-2 border-sky-600">TypeScript</strong>,{' '}
+         <strong className="border-b-2 border-sky-600">React Router</strong>,{' '}
+        <strong className="border-b-2 border-sky-600">HTML5</strong>,{' '}
+        <strong className="border-b-2 border-sky-600">CSS3</strong>,{' '}
+        <strong className="border-b-2 border-sky-600">Tailwind CSS</strong>,{' '}
+        <strong className="border-b-2 border-sky-600">Vite</strong>,{' '}
+        <strong className="border-b-2 border-sky-600">Git/GitHub</strong>,{' '}
+        <strong className="border-b-2 border-sky-600">UX/UI</strong>,{' '}
+        <strong className="border-b-2 border-sky-600">Responsividade</strong>,{' '}
+        <strong className="border-b-2 border-sky-600">Componentização</strong>,{' '}
+        <strong className="border-b-2 border-sky-600">Acessibilidade (A11y)</strong>,{' '}
+        <strong className="border-b-2 border-sky-600">JSON</strong>,{' '}
+        <strong className="border-b-2 border-sky-600">Clean Code</strong>
+        </p>   
+
+            <button className='flex items-end text-lg shadow-[0_0_5px_#38bdf8] border border-sky-800 px-2 py-1 mt-4
+        rounded-lg transition-all duration-300 hover:rounded-md hover:text-sky-400 hover:shadow-[0_0_10px_#38bdf8]'>
+            <i className='bx bx-link-external m-auto p-1'></i> 
+            <a href="https://ecommerce-store-op.netlify.app/" target="_blank">Websit</a></button>
+        </div>
+
+
+    </div>
 
 
     </div>
